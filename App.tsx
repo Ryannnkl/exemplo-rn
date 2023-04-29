a93471ddcb910478743e6bb9f7cb4c68ce58ea97
Ryannnkl <ryannnkl@gmail.com>
2023-04-29 16:05:55 -0300

    push

--- App.tsx
+++ App.tsx
@@ -1,9 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-if(__DEV__) {
-  import("./ReactotronConfig");
-}
-
 
 export default function App() {
     return (
